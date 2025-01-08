4b30af73237d3162d6e066983a3d042d0a6fc423
edramos-lab <edgar.rene.ramos.acosta@uabc.edu.mx>
2025-01-07 16:51:02 -0800

    project_version added as input paramter

--- dataset.py
+++ dataset.py
@@ -1,15 +1,20 @@
 from roboflow import Roboflow
+import sys
 
-def download_dataset():
+def download_dataset(project_version):
     rf = Roboflow(api_key="wv1RWb130ECTfhxSNxPS")
     project = rf.workspace("footdiseaseimgclass").project("things-jam67")
-    version = project.version(1)
+    version = project.version(project_version)
     dataset = version.download("folder")
     return dataset
 
-def main():
-    dataset = download_dataset()
+def main(project_version):
+    dataset = download_dataset(project_version)
     print("Dataset downloaded successfully")
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python dataset.py <ProjectVersion>")
+        sys.exit(1)
+    project_version = int(sys.argv[1])
+    main(project_version)
